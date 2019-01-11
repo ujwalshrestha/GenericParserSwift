a7eea07f248e8f4bdd962a2a59ef5e9f9cb8a10d
Redeem <redeem@redeems-imac.local>
2019-01-11 10:47:10 +0545

    Initial Commit

---
+++ GenericParseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericParseDemo
+//
+//  Created by Ujjwal's iMac on 1/11/19.
+//  Copyright © 2019 Redeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
